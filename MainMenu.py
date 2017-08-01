d21def41b736cd983fd2bb9bff205c3a3e28d57b
Nikodem Ciolczyk <ciolczyk.nikodem@gmail.com>
2017-08-01 13:44:52 +0200

    main function

--- MainMenu.py
+++ MainMenu.py
@@ -2,35 +2,41 @@ import os, time
 
 
 def load_countries_and_capitals():
+
     global a
     a = []
+
     for line in open("countries_and_capitals", 'r').readlines():
         temp = []
         temp.append(line.split(" | ")[0].upper())
         temp.append(line.split(" | ")[1].replace('\n', "").upper())
         a.append(temp)
         print(temp.__str__())
+
     print (time.strftime('%X %x'))
 
 
 def draw_menu_scene(badAnswer):
+
     os.system('clear')
     scene = open("MainMenuScene", 'r').readlines()
+
     for line in scene:
         print (line, end='')
+
     while(True):
         choice = None
-        if(badAnswer == False):
+        if badAnswer == False:
             choice = input("Your Choice: ")
-        if(badAnswer == True):
+        if badAnswer == True:
             choice = input("Bad Choice, Try Again: ")
-        if(choice == "1"):
+        if choice == "1":
             ...
-        elif(choice == "2"):
+        elif choice == "2":
             ...
-        elif(choice == "3"):
+        elif choice == "3":
             ...
-        elif(choice == "4"):
+        elif choice == "4":
             draw_quit_scene()
             exit()
 
@@ -39,16 +45,18 @@ def draw_menu_scene(badAnswer):
 
 
 def draw_quit_scene():
+
     os.system('clear')
     scene = open("GameQuitScene", 'r').readlines()
+
     for line in scene:
         print (line, end='')
 
-if __name__ == '__main__':
+
+def main():
     # draw_menu_scene(False)
     load_countries_and_capitals()
 
 
-
-
-
+if __name__ == '__main__':
+    main()
