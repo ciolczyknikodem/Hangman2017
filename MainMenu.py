b65a895974c3abc64639899aaf59cd9998182ce3
siemaeniu500 <siemaeniu500@gmail.com>
2017-08-01 13:36:04 +0200

    test

---
+++ MainMenu.py
@@ -0,0 +1,54 @@
+import os, time
+
+
+def load_countries_and_capitals():
+    global a
+    a = []
+    for line in open("countries_and_capitals", 'r').readlines():
+        temp = []
+        temp.append(line.split(" | ")[0].upper())
+        temp.append(line.split(" | ")[1].replace('\n', "").upper())
+        a.append(temp)
+        print(temp.__str__())
+    print (time.strftime('%X %x'))
+
+
+def draw_menu_scene(badAnswer):
+    os.system('clear')
+    scene = open("MainMenuScene", 'r').readlines()
+    for line in scene:
+        print (line, end='')
+    while(True):
+        choice = None
+        if(badAnswer == False):
+            choice = input("Your Choice: ")
+        if(badAnswer == True):
+            choice = input("Bad Choice, Try Again: ")
+        if(choice == "1"):
+            ...
+        elif(choice == "2"):
+            ...
+        elif(choice == "3"):
+            ...
+        elif(choice == "4"):
+            draw_quit_scene()
+            exit()
+
+        else:
+            draw_menu_scene(True)
+
+
+def draw_quit_scene():
+    os.system('clear')
+    scene = open("GameQuitScene", 'r').readlines()
+    for line in scene:
+        print (line, end='')
+
+if __name__ == '__main__':
+    # draw_menu_scene(False)
+    load_countries_and_capitals()
+
+
+
+
+
